b9fea21dc2d55da29d172a706a6afea241f066cc
Rifky Galuh <rfqgal@users.noreply.github.com>
2021-02-12 15:40:01 +0700

    Initial commit

---
+++ admin/index.php
@@ -0,0 +1,67 @@
+<?php
+  session_start();
+  if($_SESSION['level'] != "admin") {
+    header('location:../index.php');
+  }
+?>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>KanesaKu</title>
+  <link rel="stylesheet" href="../assets/style.css">
+  <style>
+  </style>
+</head>
+
+<body class="flex">
+  <nav class="sider-admin">
+    <header class="m-16">
+      <a href="index.php">
+        <img class="center" src="../assets/img/admin/Logo.svg" alt="Logo">
+      </a>
+    </header>
+    <section class="m-16">
+      <a href="#">
+        <div class="nav admin flex mb-12">
+          <img src="../assets/img/admin/Presensi Kehadiran.svg" alt="Presensi">
+          <p>Presensi Kehadiran</p>
+        </div>
+      </a>
+      <a href="#">
+        <div class="nav admin flex mb-12">
+          <img src="../assets/img/admin/Prakerin.svg" alt="Prakerin">
+          <p>Prakerin</p>
+        </div>
+      </a>
+      <a href="#">
+        <div class="nav admin flex mb-12">
+          <img src="../assets/img/admin/Rapor Siswa.svg" alt="Rapor">
+          <p>Rapor Siswa</p>
+        </div>
+      </a>
+      <a href="./account/">
+        <div class="nav admin flex mb-12">
+          <img src="../assets/img/admin/Buat Akun.svg" alt="Rapor">
+          <p>Manajemen Akun</p>
+        </div>
+      </a>
+    </section>
+    <div class="copyright t-center w-100">
+      <p>Rifky Galuh &copy; 24, XII RPL 1</p>
+    </div>
+  </nav>
+  <main>
+    <header>
+      <h1>Dashboard</h1>
+      <div class="account">
+
+      </div>
+    </header>
+    <article class="shadow-2">
+    </article>
+  </main>
+</body>
+
+</html>
---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+$server = "localhost";
+$username = "root";
+$password = "";
+$db = "kanesaku";
+
+GLOBAL $conn;
+$conn = mysqli_connect($server, $username, $password, $db);
+if (!$conn) {
+    die('Connection failed: ' . mysqli_connect_error());
+}
+?>
